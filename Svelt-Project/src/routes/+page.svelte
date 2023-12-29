967268028a32c38afe6c9d53b6c6c4dfeaad1960
Matheus Duarte <84824460+plmdsmatheus@users.noreply.github.com>
2023-12-29 03:56:20 +0000

    Tome: Get themes funcionando

--- Svelt-Project/src/routes/+page.svelte
+++ Svelt-Project/src/routes/+page.svelte
@@ -1,1 +1,39 @@
-
+<script>
+    import { onMount } from 'svelte';
+  
+    let themes = [];
+  
+    onMount(async () => {
+      try {
+        // Faz a requisição para a API
+        const response = await fetch('http://moss.ifrn.edu.br/api/themes/');
+        
+        // Verifica se a requisição foi bem-sucedida (status code 200)
+        if (response.ok) {
+          // Converte a resposta para JSON
+          themes = await response.json();
+        } else {
+          console.error('Erro ao obter os temas da API');
+        }
+      } catch (error) {
+        console.error('Erro ao realizar a requisição:', error);
+      }
+    });
+  </script>
+  
+  <main>
+    <h1>Lista de Temas</h1>
+    {#if themes.length > 0}
+      <ul>
+        {#each themes as theme (theme.id)}
+          <li>{theme.name}</li>
+        {/each}
+      </ul>
+    {:else}
+      <p>Carregando temas...</p>
+    {/if}
+  </main>
+  
+  <style>
+    
+  </style>
